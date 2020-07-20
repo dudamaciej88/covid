a42acd966cabf6254c48137587db9a684c0c0b8b
dudamaciej88 <dudamaciej88@gmail.com>
2020-07-20 13:03:45 +0200

    Add countriesArr

--- src/components/countrySelector/countrySelector.js
+++ src/components/countrySelector/countrySelector.js
@@ -1,32 +1,67 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { fetchStats, statsReset } from "../../data/actions/stats.actions";
+import * as S from "./countrySelector.style";
 
-const CountrySelector = ({selectorNumber}) => {
-    const dispatch = useDispatch()
-    
-    const changeHandler = (value, selectorNumber) => {
-        if (value.target.value !== "Choose") {
-            dispatch(fetchStats(value.target.value, selectorNumber))
-        } else {
-            dispatch(statsReset(selectorNumber));
-        }
+const CountrySelector = ({ selectorNumber }) => {
+  const dispatch = useDispatch();
+
+  const changeHandler = (value, selectorNumber) => {
+    if (value.target.value !== "Choose") {
+      dispatch(fetchStats(value.target.value, selectorNumber));
+    } else {
+      dispatch(statsReset(selectorNumber));
     }
+  };
 
+  const countriesArr = [
+    "Austria",
+    "Belarus",
+    "Belgium",
+    "Croatia",
+    "Czech Republic",
+    "Denmark",
+    "Estonia",
+    "Finland",
+    "France",
+    "Germany",
+    "Greece",
+    "Hungary",
+    "Italy",
+    "Latvia",
+    "Netherlands",
+    "Norway",
+    "Ukraine",
+    "United Kingdom",
+    "Poland",
+    "Portugal",
+    "Romania",
+    "Russia",
+    "Slovakia",
+    "Slovenia",
+    "Spain",
+    "Sweden",
+    "Switzerland",
+  ];
+
+  const options = countriesArr.map((e, key) => (
+    <option key={key} value={e}>
+      {e}
+    </option>
+  ));
   return (
-    <div>
-
-    
-
-      <label htmlFor="country">Country : </label>
-      <select name="country" onChange={(value) => changeHandler(value, selectorNumber)}>
-      <option value="Choose">Choose</option>
-        <option value="Poland">Poland</option>
-        <option value="Germany">Germany</option>
-        <option value="Italy">Italy</option>
-        <option value="Spain">Spain</option>
-      </select>
-    </div>
+    <S.Wrapper>
+      <S.Label htmlFor="country">Choose country : </S.Label>
+      <S.Select
+        name="country"
+        onChange={(value) => changeHandler(value, selectorNumber)}
+      >
+        <option value="Choose">
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-----&nbsp;&nbsp;&darr;
+        </option>
+        {options}
+      </S.Select>
+    </S.Wrapper>
   );
 };
 
