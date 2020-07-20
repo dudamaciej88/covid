f81e4344c1ccf949808e87da021f087ad44eafe6
dudamaciej88 <dudamaciej88@gmail.com>
2020-07-20 13:05:52 +0200

    Add chart configuration

--- src/components/barStats/barStats.js
+++ src/components/barStats/barStats.js
@@ -2,7 +2,7 @@ import React, { useRef, useEffect } from "react";
 import Chart from "chart.js";
 import { useSelector } from "react-redux";
 
-let myChart;
+let myBar;
 
 const BarStats = () => {
   const chartRef = useRef(null);
@@ -14,54 +14,65 @@ const BarStats = () => {
       const keysArr = Object.keys(stats[n].timeline);
       let dataArr = [];
       console.log(keysArr);
-      console.log(Math.max(...Object.values(stats[n].timeline["cases"])))
+      console.log(Math.max(...Object.values(stats[n].timeline["cases"])));
       keysArr.forEach((e) =>
         dataArr.push(Math.max(...Object.values(stats[n].timeline[e])))
-        
       );
-      
-      
       return dataArr;
-      
     } else {
       return [0, 0, 0];
     }
   };
 
-  
-
+  const labelHandler = (n) => {
+    if (Object.keys(stats[n]).length > 0) {
+      return stats[n].country;
+    } else {
+      return `Country ${n}`;
+    }
+  };
 
   useEffect(() => {
-    
     const myChartRef = chartRef.current.getContext("2d");
-    if (typeof myChart !== "undefined") myChart.destroy();
-    myChart = new Chart(myChartRef, {
+    if (typeof myBar !== "undefined") myBar.destroy();
+    Chart.defaults.global.animation.duration = 2000;
+    Chart.defaults.global.animation.easing = "linear";
+    Chart.defaults.global.defaultFontColor = 'white';
+    Chart.defaults.global.defaultFontFamily = "'Montserrat'"
+    myBar = new Chart(myChartRef, {
       type: "bar",
       data: {
         //Bring in data
-        labels: ["cases", "deaths", "recovered"],
+        labels: ["Cases", "Deaths", "Recovered"],
         datasets: [
           {
-            label: "Sales",
+            label: labelHandler(1),
             data: dataStats(1),
+            backgroundColor: '#faee1c'
           },
           {
-            label: "Sales",
+            label: labelHandler(2),
             data: dataStats(2),
+            backgroundColor: '#0e153a'
           },
         ],
-        
       },
-      options: {},
+      options: {
+        scales: {
+          yAxes: [{
+              ticks: {
+                  suggestedMin: 0,
+                  
+              }
+          }]
+      }
+      },
     });
   });
 
-  
-  
-
   return (
     <>
-      <canvas id="myChart" ref={chartRef} />
+      <canvas   id="myBar" ref={chartRef} />
     </>
   );
 };
---
+++ src/components/timelineStats/timelineGraph.js
@@ -0,0 +1,137 @@
+import React, { useRef, useEffect } from "react";
+import Chart from "chart.js";
+import { useSelector } from "react-redux";
+
+let myChart;
+
+const TimelineStats = () => {
+  const chartRef1 = useRef(null);
+
+  const stats = useSelector((state) => state.stats.stats);
+  const legend = useSelector((state) => state.legend.legend);
+
+  const labelsHandler = () => {
+    if (Object.keys(stats[1]).length > 0) {
+      return Object.keys(stats[1].timeline.cases);
+    } else if (Object.keys(stats[2]).length > 0) {
+      return Object.keys(stats[2].timeline.cases);
+    } else {
+      return ["Date"];
+    }
+  };
+
+  const dataHandler = (n, whatData) => {
+    if (Object.keys(stats[n]).length > 0) {
+      return Object.values(stats[n].timeline[whatData]);
+    } else {
+      return [];
+    }
+  };
+
+  const labelHandler = (n, name) => {
+    if (Object.keys(stats[n]).length > 0) {
+      return `${stats[n].country} ${name}`;
+    } else {
+      return `Country ${n} ${name}`;
+    }
+  };
+
+  useEffect(() => {
+    const myChartRef1 = chartRef1.current.getContext("2d");
+    if (typeof myChart !== "undefined") myChart.destroy();
+    Chart.defaults.global.animation.duration = 2000;
+    Chart.defaults.global.animation.easing = "linear";
+    Chart.defaults.global.defaultFontColor = "white";
+    
+    Chart.defaults.global.elements.line.tension = 0;
+   
+    myChart = new Chart(myChartRef1, {
+      type: "line",
+      data: {
+        //Bring in data
+        labels: labelsHandler(),
+        datasets: [
+          legend.show["cases"]
+            ? {
+                label: labelHandler(1, "cases"),
+                data: dataHandler(1, "cases"),
+                borderColor: "#faee1c",
+                pointRadius: 0,
+              }
+            : {},
+          legend.show["cases"]
+            ? {
+                label: labelHandler(2, "cases"),
+                data: dataHandler(2, "cases"),
+                borderColor: "#0e153a",
+                pointRadius: 0,
+              }
+            : {},
+          legend.show["deaths"]
+            ? {
+                label: labelHandler(1, "deaths"),
+                data: dataHandler(1, "deaths"),
+                borderColor: "green",
+                pointRadius: 0,
+              }
+            : {},
+          legend.show["deaths"]
+            ? {
+                label: labelHandler(2, "deaths"),
+                data: dataHandler(2, "deaths"),
+                borderColor: "purple",
+                pointRadius: 0,
+              }
+            : {},
+          legend.show["recovered"]
+            ? {
+                label: labelHandler(1, "recovered"),
+                data: dataHandler(1, "recovered"),
+                borderColor: "orange",
+                pointRadius: 0,
+              }
+            : {},
+          legend.show["recovered"]
+            ? {
+                label: labelHandler(2, "recovered"),
+                data: dataHandler(2, "recovered"),
+                borderColor: "white",
+                pointRadius: 0,
+              }
+            : {},
+        ],
+      },
+      options: {
+        legend: {display: false},
+        responsive: true,
+  maintainAspectRatio: true,
+        scales: {
+          xAxes: [{
+            
+            gridLines: {
+                display: false,
+                drawBorder: false
+            }
+        }],
+          yAxes: [
+            {
+              ticks: {
+                
+                suggestedMin: 0,
+              },
+              position: "right",
+            },
+          ],
+        },
+      },
+    });
+  });
+
+  return (
+    <>
+      <canvas id="myChart" ref={chartRef1} />
+    </>
+  );
+};
+
+export default TimelineStats;
