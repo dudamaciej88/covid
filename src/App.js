ac256c3ec1869f04ae6d55aaa31aa436c3373363
dudamaciej88 <dudamaciej88@gmail.com>
2020-07-20 13:04:44 +0200

    Add styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from "react";
-import "./App.css";
+import * as S from "./App.style";
+import HeaderContainer from "./components/headerContainer/headerContainer"
 import BarStats from "./components/barStats/barStats";
 import CountrySelector from "./components/countrySelector/countrySelector";
+import TimelineStats from "./components/timelineStats/timelineStats";
+import Navigation from "./components/navigation/navigation";
+import {useSelector} from "react-redux";
+import Overall from "./components/overall/overall";
 
 function App() {
+
+  const display = useSelector((state) => state.displayer.display)
+  
   return (
-    <div className="App">
+    <S.App>
+      <HeaderContainer />
+      <S.SelectorsWrapper>
       <CountrySelector selectorNumber={1} />
       <CountrySelector selectorNumber={2}/>
-      <BarStats />
-    </div>
+      </S.SelectorsWrapper>
+      <Navigation />
+      {display === "overall" ? <Overall /> : null}
+      {display === "bar" ? <BarStats /> : null}
+      {display === "timeline" ? <TimelineStats /> : null}
+    </S.App>
   );
 }
 
---
+++ src/App.style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const App = styled.div`
+background-color: #3d5af1;
+min-height: 100vh;
+max-width: 1200px;
+margin: 0px auto;
+`
+
+export const SelectorsWrapper = styled.div`
+display: flex;
+padding: 1rem;
+justify-content: space-between;
+`
