7880ea5941bcb183ed666cb666475f6c63bb71f0
dudamaciej88 <dudamaciej88@gmail.com>
2020-07-20 13:16:34 +0200

    Add legend and displayer

--- src/data/reducers/index.js
+++ src/data/reducers/index.js
@@ -1,9 +1,13 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from "redux";
 
-import stats from './stats.reducer';
+import stats from "./stats.reducer";
+import legend from "./legend.reducer";
+import displayer from "./displayer.reducer";
 
 const rootReducer = combineReducers({
-    stats,
-})
+  stats,
+  legend,
+  displayer,
+});
 
 export default rootReducer;
