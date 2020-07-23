7bcd295328cd8280e8480c56f5acfdffbc11aaf9
dudamaciej88 <dudamaciej88@gmail.com>
2020-07-24 00:47:02 +0200

    Add theming for colors and fonts

---
+++ src/themes/mainTheme.js
@@ -0,0 +1,11 @@
+export default {
+    colors: {
+      primary: "#3D5AF1",
+      secondary: "#0E153A",
+      third: "#D7DA48",
+      textColor: "#FFFFFF",
+    },
+    fonts: {
+        primary: "Montserrat",
+    }
+  };
